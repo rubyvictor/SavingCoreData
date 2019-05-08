1e748bc828933846a2256d6239c5fd5255ff47e9
victor lee ming yew <victor_li@flyscoot.com>
2019-05-08 17:15:06 +0800

    Create VC and PersistenceService

---
+++ SavingCoreData/PersistenceService.swift
@@ -0,0 +1,47 @@
+//
+//  PersistenceService.swift
+//  SavingCoreData
+//
+//  Created by Victor Lee on 8/5/19.
+//  Copyright © 2019 VictorLee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class PersistenceService {
+    
+    private init() {}
+    
+    static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    static var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "SavingCoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    static func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}
--- SavingCoreData/ViewController.swift
+++ SavingCoreData/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var people = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.backgroundColor = .blue
     }
 
+    @IBAction func didClickPlusButton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Add Person", message: "message lah", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Name"
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Age"
+            textField.keyboardType = .numberPad // for letters only
+        }
+        
+        let action = UIAlertAction(title: "Post", style: .default) { (_) in
+            
+            let name = alert.textFields?.first?.text
+            let age = alert.textFields?.last?.text
+            print("name\(name)", "age\(age)")
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
 }
 
